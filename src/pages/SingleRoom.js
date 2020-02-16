d73b434524f64bad67cbdad3c7be1ff675e90626
Eric Nguyen <enguyen89141@gmail.com>
2020-02-15 16:35:40 -0800

    added details and info. Also updated pictures to not repeat mainImg

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -22,6 +22,7 @@ export default class SingleRoom extends Component {
             </div>
         }
         const { name, description, capacity, size, price, extras, breakfast, pets, images } = room;
+        const [mainImg, ...defaultImg] = images;
         return (
             <>
                 <StyledHero img={images[0] || this.state.defaultBcg}>
@@ -33,9 +34,23 @@ export default class SingleRoom extends Component {
                 </StyledHero>
                 <section className="single-room">
                     <div className="single-room-images">
-                        {images.map((item, index) => {
+                        {defaultImg.map((item, index) => {
                             return <img key={index} src={item} alt={name} />
                         })}
+                    </div>
+                    <div className="single-room-info">
+                        <article className="desc">
+                            <h3>Details</h3>
+                            <p>{description}</p>
+                        </article>
+                        <article className="info">
+                            <h3>info</h3>
+                            <h6>Price: ${price}</h6>
+                            <h6>Size: ${size} SQFT</h6>
+                            <h6>Max capacity: {
+                                capacity > 1 ? `${capacity} people`:
+                                `${capacity} person`}</h6>
+                        </article>
                     </div>
                 </section>
             </>
