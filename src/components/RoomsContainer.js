3820b852c01f79d967a714bdbfe8351a868049e3
Eric Nguyen <enguyen89141@gmail.com>
2020-02-18 00:05:33 -0800

    updated rooms container to be more efficient through context and finished roomslist

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,27 +1,20 @@
 import React from 'react'
 import RoomsFilter from './RoomsFilter';
 import RoomsList from './RoomsList';
-import { RoomConsumer } from '../context';
+import { withRoomConsumer, RoomConsumer } from '../context';
 import Loading from './Loading';
 
-export default function RoomsContainer() {
+function RoomsContainer({ context }) {
+    const { loading, sortedRooms, rooms } = context;
+    if (loading) {
+        return <Loading />;
+    }
     return (
-        <RoomConsumer>
-            {
-                value => {
-                    const {loading, sortedRooms, rooms} = value
-                    if(loading){ 
-                        return <Loading />
-                    }
-                    return (
-                        <div>
-                            Hello from rooms containers
-                            <RoomsFilter rooms={rooms}/>
-                            <RoomsList rooms={sortedRooms}/>
-                        </div>
-                    )
-                }
-            }
-        </RoomConsumer>
+        <div>
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+        </div>
     )
 }
+
+export default withRoomConsumer(RoomsContainer)
