267651906836b7687b80ccf0541ecdad747071d6
Eric Nguyen <enguyen89141@gmail.com>
2020-02-18 01:34:52 -0800

    updated roomsFilter need to create function that only returns unique values from rooms context

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useContext } from 'react';
+import { RoomContext, RoomConsumer } from '../context';
+import Title from '../components/Title';
 
-export default function RoomsFilter() {
+export default function RoomsFilter({ rooms }) {
+    const context = useContext(RoomContext);
+    const {handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets} = context;
     return (
-        <div>
-            hello from rooms filter
-        </div>
+        <section className="filter-container">
+            <Title title="search rooms" />
+            <form className="filter-form">
+                {/*select type */}
+                <div className="form-group">
+                    <label htmlFor="type">room type</label>
+                    <select className="form-control" name="type" id="type" value={type} onChange={handleChange}>
+                        
+                    </select>
+                </div>
+                {/*end select type */}
+            </form>
+        </section>
     )
 }
--- src/context.js
+++ src/context.js
@@ -8,16 +8,30 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type: 'all',
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     };
     componentDidMount() {
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price))
+        let maxSize = Math.max(...rooms.map(item => item.size))
         this.setState({
             rooms,
             featuredRooms,
             sortedRooms: rooms,
-            loading: false
+            loading: false,
+            price: maxPrice,
+            maxPrice,
+            maxSize: maxSize
         })
     }
     formatData(items) {
@@ -34,11 +48,21 @@ class RoomProvider extends Component {
         const room = tempRooms.find(room => room.slug === slug);
         return room
     }
+    handleChange = e => {
+        const type = e.target.type
+        const name = e.target.name
+        const value = e.target.value
+        console.log(type, name, value);
+    }
+    filterRooms = () => {
+        console.log('filter')
+    }
     render() {
         return (
             <RoomContext.Provider value={{
                 ...this.state,
-                getRoom: this.getRoom
+                getRoom: this.getRoom,
+                handleChange: this.handleChange,
             }}>
                 {this.props.children}
             </RoomContext.Provider>
